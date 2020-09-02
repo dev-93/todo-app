8eb707022235ed4b1b41e95ff21fb3b12773aed3
dev-93 <cjb940103@gmail.com>
2020-09-03 00:48:00 +0900

    UI - TodoTemplate 스타일 생성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import "./App.css";
+import TodoTemplate from "./components/TodoTemplate";
 
 function App() {
-    return <div className="App">Todo App을 만들자</div>;
+    return <TodoTemplate>Todo App을 만들자</TodoTemplate>;
 }
 
 export default App;
