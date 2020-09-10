cb227c5b41111296ff121b859ba24a70b7ca9ad7
dev-93 <cjb940103@gmail.com>
2020-09-11 01:41:33 +0900

    지우기 기능

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./TodoList.scss";
 import TodoListItem from "./TodoListItem";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
     return (
         <div className="TodoList">
             {todos.map((todo) => {
-                return <TodoListItem todo={todo} key={todo.id} />;
+                return (
+                    <TodoListItem
+                        todo={todo}
+                        key={todo.id}
+                        onRemove={onRemove}
+                    />
+                );
             })}
         </div>
     );
