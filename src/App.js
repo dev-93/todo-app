fa736483261b1f1b4447702f3f5283cb5d0f9a90
dev-93 <cr000@naver.com>
2020-10-14 00:08:38 +0900

    오류 잡는 중

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TodoList from "./components/TodoList";
 
 function createBulkTodos() {
     const array = [];
-    for (let i = 1; i <= 2500; i++) {
+    for (let i = 1; i <= 100; i++) {
         array.push({
             id: 1,
             text: `할일 ${i}`,
@@ -20,24 +20,22 @@ function App() {
 
     const nextId = useRef(4);
 
-    const onInsert = useCallback(
-        (text) => {
-            const todo = {
-                id: nextId.current,
-                text,
-                checked: false,
-            };
-            setTodos(todos.concat(todo));
-            nextId.current += 1;
-        },[]);
+    const onInsert = useCallback(text => {
+        const todo = {
+            id: nextId.current,
+            text,
+            checked: false,
+        };
+        setTodos(todos => todos.concat(todo));
+        nextId.current += 1;
+    }, []);
 
-    const onRemove = useCallback(
-        (id) => {
-            setTodos(todos.filter((todo) => todo.id !== id));
-        },[]);
+    const onRemove = useCallback(id => {
+        setTodos(todos => todos.filter((todo) => todo.id !== id));
+    }, []);
 
     const onToggle = useCallback(id => {
-        setTodos( todos =>
+        setTodos(todos =>
             todos.map((todo) =>
                 todo.id === id ? { ...todo, checked: !todo.checked } : todo,
             ),
